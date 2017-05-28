d9075e06d6d12c106dff71c3b8f35cc20574cc7c
Hanrich <Hanrichp@gmail.com>
2017-05-28 21:19:01 +0200

    fixed broken hyperlinks

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from "angularfire2/database/database";
+import { Subscription } from "rxjs";
+import {subscribeOn} from "rxjs/operator/subscribeOn";
 
 @Injectable()
 export class UserService {
 	loggedInUser;
+	subscriptions : Subscription[] = [];
 
-  constructor(public afAuth: AngularFireAuth) { 
-	afAuth.auth.onAuthStateChanged(function(user) {
-		if(user){
-			this.loggedInUser = user;
-		}
-		else{
-			this.loggedInUser = undefined;
-		}
-	});
+  constructor(
+    public afAuth: AngularFireAuth,
+    public db: AngularFireDatabase
+  ) {
+
+    afAuth.auth.onAuthStateChanged((user) => {
+      if(user){
+        this.loggedInUser = user;
+        this.getUserInformation();
+      }
+      else{
+        this.loggedInUser = undefined;
+      }
+    });
+
   }
 
   logIn(username : string , password : string) {
@@ -26,11 +36,10 @@ export class UserService {
      		console.log(error);
    		})
   	});
-  	
-
   }
 
   logOut() {
+    this.cancelSubscriptions();
   	this.afAuth.auth.signOut();
   	this.loggedInUser = null;
   }
@@ -39,4 +48,23 @@ export class UserService {
   	return this.loggedInUser ? true : false;
   }
 
+  getUserInformation(){
+    if(this.loggedInUser) {
+
+      let userInformationObservable = this.db.object(`/invites/${this.loggedInUser.uid}`);
+
+      this.subscriptions.push(userInformationObservable.subscribe(userInfromation => {
+          console.log("User Info",userInfromation);
+      }));
+
+    }
+  }
+
+  cancelSubscriptions(){
+    this.subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    });
+  }
+
 }
+
